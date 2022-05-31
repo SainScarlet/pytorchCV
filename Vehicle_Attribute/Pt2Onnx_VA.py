8708d592687cd10c8a4cb9d1eff106f20a213901
SainScarlet <acid.fox0@gmail.com>
2022-05-31 21:32:49 +0800

    更新多输出写法命名

--- Vehicle_Attribute/Pt2Onnx_VA.py
+++ Vehicle_Attribute/Pt2Onnx_VA.py
@@ -10,6 +10,7 @@ model.eval()
 dummy_input = t.randn(1, 3, 64, 64)
 # 最好在函数中指定in和out的名字，防止netron显示时产生误会，特别示output
 # 注意in out的写法，需加入[]
+# 注意多输出的写法
 # t.onnx.export(model, dummy_input, './0_model/02_cnn_mnist_model.onnx',
 #               verbose=True, input_names=['ImgInput'], output_names=['PredictNumber'])
 t.onnx.export(model, dummy_input, './model/vehicle_attributes_demo.onnx',
