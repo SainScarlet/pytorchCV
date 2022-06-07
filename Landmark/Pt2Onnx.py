51e07bb2e31e8ff0f4aea62494b4aa02125bcc38
SainScarlet <acid.fox0@gmail.com>
2022-06-07 16:03:48 +0800

    新增完整模型转onnx，需要在工作站测试一下

--- Landmark/Pt2Onnx.py
+++ Landmark/Pt2Onnx.py
@@ -1,16 +1,24 @@
 import torch as t
-from Landmark_CNN import Net
+# from Landmark_CNN import Net
+from Landmark_CNN_GPU import Net
 
-model = Net()
+# model = Net()
 
-model.load_state_dict(t.load('./model/landmark.pt'))
-model.eval()
+# # 字典数据的加载
+# model.load_state_dict(t.load('./model/landmark.pt'))
+# model.eval()
+
+# 完整模型的加载
+# M1加载CUDA训练完的模型有点问题，需要尝试
+model = t.load('./model/landmark_full.pt')
+model.cpu()
 
 # dummy一个虚假的输入数据格式
 dummy_input = t.randn(1, 3, 64, 64)
 # 最好在函数中指定in和out的名字，防止netron显示时产生误会，特别示output
 # 注意in out的写法，需加入[]
-# t.onnx.export(model, dummy_input, './0_model/02_cnn_mnist_model.onnx',
-#               verbose=True, input_names=['ImgInput'], output_names=['PredictNumber'])
-t.onnx.export(model, dummy_input, './model/landmark.onnx',
+# t.onnx.export(model, dummy_input, './model/landmark.onnx',
+#               verbose=True, input_names=['ImgInput'], output_names=['LandmarkPoint'])
+
+t.onnx.export(model, dummy_input, './model/landmark_test.onnx',
               verbose=True, input_names=['ImgInput'], output_names=['LandmarkPoint'])
