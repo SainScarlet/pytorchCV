2413acf6a7c1548c30e1f6f16ddf5ee812b35384
SainScarlet <acid.fox0@gmail.com>
2022-06-17 22:27:43 +0800

    修改部分代码，有两个warning，还需要再修改

--- Vehicle_Attribute/VehicleAttribute_predict.py
+++ Vehicle_Attribute/VehicleAttribute_predict.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 
 import cv2 as cv
 import numpy as np
-# import torch
+import torch
 import logging as log
 from VehicleAttribute_model import ResidualBlock, VehicleAttributesResNet
 from openvino.inference_engine import IENetwork, IECore
@@ -12,26 +12,32 @@ from openvino.inference_engine import IENetwork, IECore
 color_labels = ["white", "gray", "yellow", "red", "green", "blue", "black"]
 type_labels = ["car", "bus", "truck", "van"]
 
-model_dir = "/Users/remilia/Documents/02-Work/05-Python/02_pytorchCV/Vehicle_Attribute/model"
+# model_dir = "/Users/remilia/Documents/02-Work/05-Python/02_pytorchCV/Vehicle_Attribute/model/"
+model_dir = "D:/2_Work/1_python/pytorchCV/Vehicle_Attribute/model/"
 model_xml = model_dir + "vehicle-detection-adas-0002.xml"
 model_bin = model_dir + "vehicle-detection-adas-0002.bin"
 
 net = IENetwork(model=model_xml, weights=model_bin)
+# net = IECore.read_network(model=model_xml, weights=model_bin)
 
 ie = IECore()
 log.info("Device info:")
 versions = ie.get_versions("CPU")
 
 # pt完整模型加载
-# cnn_model = torch.load("./vehicle_attributes_model.pt")
-# print(cnn_model)
+cnn_model = torch.load("./model/vehicle_attributes_demo.pt")
+print(cnn_model)
 
-cnn_model = cv.dnn.readNetFromONNX('./model/vehicle_attributes_demo.onnx')
+# cnn_model = cv.dnn.readNetFromONNX('./model/vehicle_attributes_demo.onnx')
 
 input_blob = next(iter(net.inputs))
 n, c, h, w = net.inputs[input_blob].shape
+print(n, c, h, w)
 
-capture = cv.VideoCapture("/Users/remilia/Documents/02-Work/05-Python/0-source/VAtest.mp4")
+# capture = cv.VideoCapture("/Users/remilia/Documents/02-Work/05-Python/0-source/VAtest.mp4")
+capture = cv.VideoCapture("D:/2_Work/1_python/0-source/VAtest.mp4")
+# capture = cv.VideoCapture(0)
+
 ih = capture.get(cv.CAP_PROP_FRAME_HEIGHT)
 iw = capture.get(cv.CAP_PROP_FRAME_WIDTH)
 
@@ -44,6 +50,9 @@ log.info("Creating infer request and starting inference")
 
 while True:
     ret, src = capture.read()
+
+    print(ret, src)
+
     if ret is not True:
         break
     images = np.ndarray(shape=(n, c, h, w))
@@ -87,22 +96,22 @@ while True:
             img = cv.resize(vehicle_roi, (72, 72))
             img = (np.float32(img) / 255.0 - 0.5) / 0.5
             img = img.transpose((2, 0, 1))
-            # x_input = torch.from_numpy(img).view(1, 3, 72, 72)
-            x_input = img.reshape(1, 3, 72, 72)
+            x_input = torch.from_numpy(img).view(1, 3, 72, 72)
+            # x_input = img.reshape(1, 3, 72, 72)
 
-            # color_, type_ = cnn_model(x_input.cuda())
-            # color_, type_ = cnn_model(x_input)
+            color_, type_ = cnn_model(x_input.cuda())
+            color_, type_ = cnn_model(x_input)
 
-            cnn_model.setInput(x_input)
-            color_, type_ = cnn_model.forward()
+            # cnn_model.setInput(x_input)
+            # color_, type_ = cnn_model.forward()
 
-            # # torch.max()[1]只返回最大值的每个索引
-            # predict_color = torch.max(color_, 1)[1].cpu().detach().numpy()[0]
-            # predict_type = torch.max(type_, 1)[1].cpu().detach().numpy()[0]
+            # torch.max()[1]只返回最大值的每个索引
+            predict_color = torch.max(color_, 1)[1].cpu().detach().numpy()[0]
+            predict_type = torch.max(type_, 1)[1].cpu().detach().numpy()[0]
 
-            # onnx
-            predict_color = color_.index(max(color_))
-            predict_type = type_.index(max(type_))
+            # # onnx
+            # predict_color = color_.index(max(color_))
+            # predict_type = type_.index(max(type_))
 
             attrs_txt = "color:%s, type:%s" % (color_labels[predict_color], type_labels[predict_type])
             cv.rectangle(src, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
